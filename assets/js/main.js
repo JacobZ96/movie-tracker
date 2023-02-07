94c9af8af442e125806dafb9c3adb5c352f96a9a
IaroslavLasiichuk <lasmant@yahoo.com>
2023-02-06 21:24:20 -0700

    Javascrip fixed bug

--- assets/js/main.js
+++ assets/js/main.js
@@ -28,7 +28,8 @@ fetch(`https://www.omdbapi.com/?apikey=d5dbe20a&s=${searchText}`)
             <p>${movies[i].Year}</p>
             <img src="${movies[i].Poster}" alt="${"Poster for"}${movies[i].Title}">
             <a href="http://imdb.com/title/${movies[i].imdbID}" target="_blank" class="btn btn-primary">View IMDB</a>
-            <button class="buttons" id=${movies[i].imdbID} data-title = ${titleForAttribute}>Add to list</button>
+            <li><button class="buttons" id=${movies[i].imdbID} data-title = ${titleForAttribute}>Add to list</button> </li>
+          
          `
         }
        
@@ -41,13 +42,15 @@ fetch(`https://www.omdbapi.com/?apikey=d5dbe20a&s=${searchText}`)
 
 function movieSelected(event) {
 let selected = event.target;
-let atrr = selected.getAttribute('data-title');
-console.log(selected.id);
-localStorage.setItem('movieId', atrr);
-console.log(localStorage.getItem('movieId'));
-let listOfMovies = document.querySelector('.list');
-let buttons = document.createElement('button');
-buttons.textContent = innerHTML = atrr.replace(/[-]/g, ' ');
-listOfMovies.appendChild(buttons);
+    let atrr = selected.getAttribute('data-title');
+    let movieId = localStorage.getItem('movieId');
+    if (atrr !== movieId ) {
+        localStorage.setItem('movieId', atrr);
+        console.log(localStorage.getItem('movieId'));
+        let listOfMovies = document.querySelector('.list');
+            let buttons = document.createElement('button');
+        buttons.textContent = innerHTML = atrr.replace(/[-]/g, ' ');
+        listOfMovies.appendChild(buttons); 
+    }
 }
 document.querySelector('#movie-info').addEventListener('click', movieSelected);
